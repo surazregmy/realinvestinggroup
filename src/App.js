8cfa32b33cfa5d5e1e042bdb13d21bc04e83ac7c
surazregmy <suraz.regmy@gmail.com>
2023-03-23 14:11:53 -0400

    first page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Form, Input } from "antd";
+import ReactTypingEffect from "react-typing-effect";
+import "./App.css";
 
 function App() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="image" style={{ height: "100vh" }}>
+        <div className="container" style={{ textAlign: "center" }}>
+          <div className="header" style={{ textAlign: "center" }}>
+            <h1 style={{ color: "white" }}>RealInvesting Group</h1>
+          </div>
+          <br></br>
+          <div className="welcome" style={{ textAlign: "center" }}>
+            <p
+              style={{ color: "white", fontSize: "50px", fontWeight: "bolder" }}
+            >
+              Welcome
+            </p>
+          </div>
+          <br></br>
+          <div>
+            <div
+              className="signupform"
+              style={{ textAlign: "center", color: "white" }}
+            >
+              <ReactTypingEffect
+                text={"#1 Choice for Investors"}
+                cursorRenderer={(cursor) => <h1>{cursor}</h1>}
+                displayTextRenderer={(text, i) => {
+                  return (
+                    <h1>
+                      {text.split("").map((char, i) => {
+                        const key = `${i}`;
+                        return (
+                          <span
+                            key={key}
+                            style={i == 0 ? { color: "green" } : {}}
+                          >
+                            {char}
+                          </span>
+                        );
+                      })}
+                    </h1>
+                  );
+                }}
+              ></ReactTypingEffect>
+              <h2>
+                Join our 100's of investors for Toronto & Calgary's Exclusive
+                Pre-construction Projects
+              </h2>
+              <h3>Enter Your Email Address!</h3>
+
+              <div className="joinform">
+                <div className="formcenter center-screen">
+                  <Form
+                    name="basic"
+                    initialValues={{
+                      remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                    style={{ minWidth: "375px" }}
+                  >
+                    <Form.Item
+                      name="username"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your email!",
+                        },
+                      ]}
+                    >
+                      <Input size="large" />
+                    </Form.Item>
+                    <Form.Item>
+                      <Button
+                        type="primary"
+                        htmlType="submit"
+                        style={{
+                          width: "375px",
+                          color: "white",
+                          backgroundColor: "green",
+                          fontSize: "20px",
+                        }}
+                        size="large"
+                      >
+                        Subscribe Now
+                      </Button>
+                    </Form.Item>
+                    <p style={{ color: "white" }}>
+                      *Unsubscribe anytime - Always free to join!
+                    </p>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
